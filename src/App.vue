02c947a313d013a260766f3660ee504e6df38609
Mr2803 <rispoli.mar2803@gmail.com>
2020-04-07 20:52:42 +0200

    now the BG changes according to the weather

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,9 @@
 <template>
-  <div id="app">
+  <div id="app" :class="typeof weather.main != 'undefined' ? setBg(weather.weather[0].main) : ''">
     <main>
       <div class="container-fluid">
         <div class="row text-center">
-          <div
-            class="col-12 mt-5 d-flex justify-content-center flex-column align-items-center"
-          >
+          <div class="col-12 mt-5 d-flex justify-content-center flex-column align-items-center">
             <div class="search">
               <input
                 type="text"
@@ -15,18 +13,24 @@
                 @keypress="fetchWeather"
               />
             </div>
-            <div class="weather-wrap" v-if="typeof weather.main != 'undefined'">
+            <div
+              class="weather-wrap"
+              v-if="typeof weather.main != 'undefined' && weather.main != ''"
+            >
               <div class="location-container">
-                <div class="location">
-                  {{ weather.name }} ,{{ weather.sys.country }}
-                </div>
-                <div class="date">
-                  {{ new Date().toLocaleDateString("it-IT", options) }}
-                </div>
+                <div class="location">{{ weather.name }} ,{{ weather.sys.country }}</div>
+                <div class="date">{{ new Date().toLocaleDateString("it-IT", options) }}</div>
               </div>
               <div class="weather-box">
                 <div class="temp">{{ Math.round(weather.main.temp) }}°c</div>
                 <div class="weather">{{ weather.weather[0].main }}</div>
+              </div>
+              <div class="wind">
+                <img src="../src/assets/wind1.png" alt />
+                {{
+                weather.wind.speed
+                }}
+                m/s
               </div>
             </div>
           </div>
@@ -49,32 +53,49 @@ export default {
         weekday: "long",
         year: "numeric",
         month: "long",
-        day: "numeric",
-      },
+        day: "numeric"
+      }
     };
   },
   methods: {
     fetchWeather(e) {
-      if (e.key == "Enter") {
+      var key = e.which || e.keyCode || 0;
+      if (key == 13) {
         fetch(
           `${this.base_url}weather?q=${this.location}&units=metric&APPID=${this.api_key}`
         )
-          .then((res) => {
+          .then(res => {
             return res.json();
           })
-          .then(this.setResults);
+          .then(this.setResults)
+          .then(this.reset);
       }
+    },
+    setBg(weather) {
+      if (weather == "Clear") return "clear";
+      else if (weather == "Clouds") return "clouds";
+      else if (weather == "Rain") return "rain";
     },
     setResults(results) {
       this.weather = results;
       console.log(this.weather);
     },
-  },
+    reset() {
+      document.querySelector(".search-bar").value = "";
+    }
+  }
 };
 </script>
 
 <style lang="scss" scoped>
 $text-color-primary: white;
+
+@mixin setBg($path) {
+  background: $path;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+
 * {
   margin: 0;
   padding: 0;
@@ -136,7 +157,8 @@ $text-color-primary: white;
       border-radius: 16px;
       box-shadow: 3px 6px rgba(0, 0, 0, 0.25);
     }
-    .weather {
+    .weather,
+    .wind {
       color: $text-color-primary;
       font-size: 50px;
       font-weight: 700;
@@ -144,5 +166,18 @@ $text-color-primary: white;
       text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
     }
   }
+}
+#app.rain {
+  @include setBg(url("assets/storm.gif"));
+}
+#app.clear {
+  @include setBg(
+    url("https://media.giphy.com/media/mEUl5b6QiFKvHPbZm3/giphy.gif")
+  );
+}
+#app.clouds {
+  @include setBg(
+    url("https://media.giphy.com/media/QU3fhTj1hc0FzofIGB/giphy.gif")
+  );
 }
 </style>
